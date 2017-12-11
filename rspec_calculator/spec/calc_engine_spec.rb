7d56bba4d441a495b023faf553d0ce36c06df9ee
Tech-A66 <tech-a66@Tech-A66s-MacBook-Air.local>
2017-12-11 17:05:58 +0000

    improved effiency

--- rspec_calculator/spec/calc_engine_spec.rb
+++ rspec_calculator/spec/calc_engine_spec.rb
@@ -2,24 +2,24 @@ require 'spec_helper'
 
 describe Calcengine do
 
+  before(:all) do
+    @calc = Calcengine.new
+  end
+
   it 'should correctly add two numbers' do
-    calc = Calcengine.new
-    expect(calc.add(1,1)).to eql(2)
+    expect(@calc.add(1,1)).to eql(2)
   end
 
   it 'should correctly subtract 2 numbers' do
-    calc = Calcengine.new
-    expect(calc.minus(2,1)).to eql(1)
+    expect(@calc.minus(2,1)).to eql(1)
   end
 
   it 'should correctly multiply 2 numbers' do
-    calc = Calcengine.new
-    expect(calc.times(2,2)).to eql(4)
+    expect(@calc.times(2,2)).to eql(4)
   end
 
   it 'should correctly divide 2 numbers' do
-    calc = Calcengine.new
-    expect(calc.divide(6,2)).to eql(3)
+    expect(@calc.divide(6,2)).to eql(3)
   end
 
 end
